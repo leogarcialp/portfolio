2946e5838de9ef9b7c2eea4a93eb6115fe4e1624
Leonardo <leogarcia986@gmail.com>
2021-04-22 01:12:36 -0600

    Dynamic year added

--- app.js
+++ app.js
@@ -2,3 +2,7 @@ const toggle = document.getElementsByClassName('header__toggle')[0];
 const navbarLinks = document.getElementsByClassName('header__links-container')[0];
 
 toggle.addEventListener('click', () => navbarLinks.classList.toggle('active'));
+
+
+// Dynamic year (Footer)
+document.getElementById("year").innerHTML = new Date().getFullYear();
