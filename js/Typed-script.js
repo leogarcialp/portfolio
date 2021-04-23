fbf2f568a6c63cfb1e020936146643b0ac571ff4
Leonardo <leogarcia986@gmail.com>
2021-04-22 19:54:13 -0600

    Type animation added to my name

---
+++ js/Typed-script.js
@@ -0,0 +1,16 @@
+const typed = new Typed('.typed',{
+   strings: ['<i class="name">nardo</i>'],
+
+
+	typeSpeed: 100, // Velocidad en mlisegundos para poner una letra,
+	startDelay: 3000, // Tiempo de retraso en iniciar la animacion. Aplica tambien cuando termina y vuelve a iniciar,
+	backSpeed: 75, // Velocidad en milisegundos para borrrar una letra,
+	smartBackspace: true, // Eliminar solamente las palabras que sean nuevas en una cadena de texto.
+	shuffle: false, // Alterar el orden en el que escribe las palabras.
+	backDelay: 1500, // Tiempo de espera despues de que termina de escribir una palabra.
+	loop: false, // Repetir el array de strings
+	loopCount: false, // Cantidad de veces a repetir el array.  false = infinite
+	showCursor: false, // Mostrar cursor palpitanto
+	cursorChar: '|', // Caracter para el cursor
+	contentType: 'html', // 'html' o 'null' para texto sin formato
+});
