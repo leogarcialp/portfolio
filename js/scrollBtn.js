8894dae265d1b30555c7993678729a075704814d
leogarcialp <leogarcia986@gmail.com>
2022-10-07 15:39:57 -0600

    design finished, logic added but needs to be fixed

--- js/scrollBtn.js
+++ js/scrollBtn.js
@@ -1,1 +1,14 @@
-// TODO: create js to show link when scrolling and bounce effect
+const scrollBtn = document.querySelector('.top-link');
+
+
+window.addEventListener('scroll', () => {
+  const location = scrollBtn.getBoundingClientRect();
+
+   console.log(location);
+
+   if(location.top < -725){
+     scrollBtn.classList.add('show-link');
+   } else {
+      scrollBtn.classList.remove('show-link');
+   }
+});
