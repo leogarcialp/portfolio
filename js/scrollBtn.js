e508e34b01bcb33e4dcb0816e9725d8975095bbf
leogarcialp <leogarcia986@gmail.com>
2022-10-07 17:12:14 -0600

    logic fixed and working

--- js/scrollBtn.js
+++ js/scrollBtn.js
@@ -1,12 +1,12 @@
 const scrollBtn = document.querySelector('.top-link');
+const scrollCount = document.querySelector('#home');
 
 
 window.addEventListener('scroll', () => {
-  const location = scrollBtn.getBoundingClientRect();
-
+   const location = scrollCount.getBoundingClientRect();
    console.log(location);
 
-   if(location.top < -725){
+   if(location.top < -800){
      scrollBtn.classList.add('show-link');
    } else {
       scrollBtn.classList.remove('show-link');
