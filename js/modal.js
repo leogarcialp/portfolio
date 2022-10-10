8a87d439ef6ed3246295870b884c95cce9fca7fa
leogarcialp <leogarcia986@gmail.com>
2022-10-09 22:59:21 -0600

    modal js  file created

---
+++ js/modal.js
@@ -0,0 +1,12 @@
+/* TODO: create an object with the description of projects 
+similar to the Reviews project.
+Existing files need to be modified
+
+-All HTML and info will be generated with JS.
+
+-include: ID, img, Title, Web stack used, description.
+
+-The modal should open and show the description
+when you click the img of the project
+*/
+
