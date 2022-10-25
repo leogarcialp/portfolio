f9e4c91777f15e7527e8cebca6622b7afd4ce318
leogarcialp <leogarcia986@gmail.com>
2022-10-24 23:19:05 -0600

    data added to JS file

--- js/modal.js
+++ js/modal.js
@@ -10,6 +10,46 @@ Existing files need to be modified
 when you click the img of the project
 */
 
+// data projects
+const info = [
+   {
+      id: 1, 
+      title: 'Portfolio',
+      desc: 'My portfolio made from scratch using HTML5, CSS3 and JavaScript. My first big project.',
+   },
+
+   {
+      id: 2, 
+      title: 'Menu SV',
+      desc: 'Order of three. The best known typical dish. A rice or corn flour tortilla filled with beans, cheese and/or chicharrón. Accompanied by "curtido" and tomato sauce.',
+   },
+
+   {
+      id: 3, 
+      title: 'Weather App',
+      desc: 'Order of three. The best known typical dish. A rice or corn flour tortilla filled with beans, cheese and/or chicharrón. Accompanied by "curtido" and tomato sauce.',
+   },
+
+   {
+      id: 4, 
+      title: 'React JS Project',
+      desc: 'Order of three. The best known typical dish. A rice or corn flour tortilla filled with beans, cheese and/or chicharrón. Accompanied by "curtido" and tomato sauce.',
+   },
+
+   {
+      id: 5, 
+      title: 'Full Stack Project',
+      desc: 'Order of three. The best known typical dish. A rice or corn flour tortilla filled with beans, cheese and/or chicharrón. Accompanied by "curtido" and tomato sauce.',
+   },
+
+   {
+      id: 6, 
+      title: 'El Salvador culture website',
+      desc: 'Order of three. The best known typical dish. A rice or corn flour tortilla filled with beans, cheese and/or chicharrón. Accompanied by "curtido" and tomato sauce.',
+   },
+];
+
+
 const modal = document.querySelector('.modal-overlay');
 const openModal = document.querySelector('.open-modal');
 const closeModal = document.querySelector('.close-modal');
