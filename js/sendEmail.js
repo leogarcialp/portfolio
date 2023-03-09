6a269f1d13c4fdd6f341990af06ed3b89de48267
leogarcialp <leogarcia986@gmail.com>
2023-03-09 00:11:28 -0600

    first steps done to  validate the form fields

--- js/sendEmail.js
+++ js/sendEmail.js
@@ -1,32 +1,32 @@
-const btnSend = document.querySelector('.contact__submit');
+// const btnSend = document.querySelector('.contact__submit');
 
-// disable Send button
-btnSend.disabled = true;
-btnSend.classList.add('cursor-not-allowed', 'opacity-50', 'bg-blue-500');
+// // disable Send button
+// btnSend.disabled = true;
+// btnSend.classList.add('cursor-not-allowed', 'opacity-50', 'bg-blue-500');
 
-// Validate form fields
-
-
+// // Validate form fields
 
 
-document.querySelector('.contact__form').addEventListener('submit', function(event) {
-   event.preventDefault();
 
-   btnSend.value = 'Sending...';
 
-   const serviceID = 'default_service';
-   const templateID = 'template_hpfiyhf';
+// document.querySelector('.contact__form').addEventListener('submit', function(event) {
+//    event.preventDefault();
 
-   emailjs.sendForm(serviceID, templateID, this)
-   .then(() => {
-         btnSend.value = 'Send Message';
-         alert('Email Sent!');
-      }, (err) => {
-         btnSend.value = 'Send Message';
-         alert(JSON.stringify(err));
-      }
-   );
-});
+//    btnSend.value = 'Sending...';
+
+//    const serviceID = 'default_service';
+//    const templateID = 'template_hpfiyhf';
+
+//    emailjs.sendForm(serviceID, templateID, this)
+//    .then(() => {
+//          btnSend.value = 'Send Message';
+//          alert('Email Sent!');
+//       }, (err) => {
+//          btnSend.value = 'Send Message';
+//          alert(JSON.stringify(err));
+//       }
+//    );
+// });
 
 
 /*TODO: 
@@ -46,4 +46,33 @@ logic proposal:
 - Disable send Message button at the beginning.  Use → (DOMContentLoaded) 
 - Validate fields and then enable SendMessage button
 - Design/adapt error messages according to the page
+*/
+
+
+// Logic based on a project
+
+document.addEventListener('DOMContentLoaded', () => {
+   // Elements
+   inputName = document.querySelector('#name');
+   inputEmail = document.querySelector('#email');
+   inputMessage = document.querySelector('#message');
+
+   // Events
+   inputName.addEventListener('blur', validate);
+   inputEmail.addEventListener('blur', validate);
+   inputMessage.addEventListener('blur', validate);
+
+   // Functions
+   function validate(e) {
+      if(e.target.value.trim() === '') {
+         showAlert(`The field ${e.target.id} is required`, e.target.parentElement);
+         return;
+      }
+   }
+
+});
+
+/* TODO: Pay attention to what is the parent element of my fields 👀👀
+- Las alertas por cada field deben ir debajo de cada field, como hijo
+usando appendChild.
 */
